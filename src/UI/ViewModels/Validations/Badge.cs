5eccaa7f9ba352cf1bedc809b26f1c9d896ced91
Igor Anjos <igor.anjos@orbium.com.br>
2020-08-29 23:17:42 -0300

    Ajuste para renomear as pastas

---
+++ src/UI/ViewModels/Validations/Badge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.ViewModels.Validations
+{
+    public class Badge
+    {
+        public static string CalculateFamilyIncome(decimal familyIncome)
+        {
+            string result = string.Empty;
+            string income = familyIncome.ToString();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(income))
+                    return result = string.Empty;
+
+
+                var _familyIncome = income.Split(",");
+
+
+                result = String.Format($"R$ {_familyIncome[0].ToString()}");
+            }
+            catch (Exception err)
+            {
+                throw new Exception($"Error: Erro no try do cálculo da renda familiar{0}", err);
+            }
+            return result;
+        }
+    }
+}
